50d33abe042305fd526d8e6c59ecd7fe7387bec7
luolanmeet <mainem@163.com>
2019-01-13 12:48:24 +0800

    补充资源，让测试代码能通过

--- pdf/itext/src/test/java/test/TestPDFCreater.java
+++ pdf/itext/src/test/java/test/TestPDFCreater.java
@@ -19,7 +19,7 @@ public class TestPDFCreater {
     public void before() {
         
         dirays = new ArrayList<Diray>();
-        user = new User("cck", "越来越开心~~~~~~~~~~~~~", "c.jpg");
+        user = new User("cck", "越来越开心~~~~~~~~~~~~~", "7.jpg");
         int i = 10;
         while(i-- > 0) {
             dirays.add(new Diray("我越是逃离，却越是靠近你；我越是背过脸，却越是看见你。我是一座孤岛，处在相思之水中。四面八方，隔绝我通向你。一千零一面镜子，转映着你的容颜，我从你开始，我在你结束。 " + i, "2014-6-18"));
