9e6f9ab483772669b902b354cfd82fd166de0609
luolanmeet <mainem@163.com>
2019-04-13 14:57:40 +0800

    add guava joiner & splitter

---
+++ guava/src/main/java/com/TestJoiner.java
@@ -0,0 +1,48 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Joiner;
+
+/**
+ * 连接字符串
+ * 和 Splitter 对应
+ * @author cck
+ */
+public class TestJoiner {
+    
+    public static void main(String[] args) {
+        
+        // 拼接字符串
+        List<String> list = new ArrayList<>();
+        list.add("A");list.add("B");list.add("C");
+        String str = Joiner.on(";").join(list);
+        System.out.println(str);
+
+        // 拼接在已有的字符串上
+        StringBuilder strbuil = new StringBuilder("Z;");
+        Joiner.on(";").appendTo(strbuil, list);
+        System.out.println(strbuil.toString());
+
+        // useForNull("str") 用str替换null
+        list = Arrays.asList("1", "2", null, "3");  // 有 null 时，调用 Joinder会报错
+        str = Joiner.on(";").useForNull("0").join(list);
+        System.out.println(str);
+        
+        // skipNulls() 忽略null
+        list = Arrays.asList("1", "2", null, "3");
+        str = Joiner.on(";").skipNulls().join(list);
+        System.out.println(str);
+        
+        //　将一个map组装成一个字符串，构建 url路径上的参数时会很好用
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "cck");map.put("password", "123456");
+        str = Joiner.on("&").withKeyValueSeparator("=").join(map);
+        System.out.println(str);
+    }
+    
+}
---
+++ guava/src/main/java/com/TestSplitter.java
@@ -0,0 +1,47 @@
+package com;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Splitter;
+
+/**
+ * 拆分字符串
+ * 和 Joiner 对应
+ * @author cck
+ */
+public class TestSplitter {
+    
+    public static void main(String[] args) {
+        
+        // 解析 url 上的参数挺好用的
+        List<String> list = Splitter.on(";").splitToList("a;b;c");
+        System.out.println(list);
+        
+        // 只解析为3个结果
+        list = Splitter.on(";").limit(3).splitToList("a;b;c;d;e");
+        System.out.println(list);
+        
+        // 只解析为3个结果
+        list = Splitter.on(";").limit(3).splitToList("a;b");
+        System.out.println(list);
+        
+        // 按照长度，切割字符串
+        list = Splitter.fixedLength(3).splitToList("1234567");
+        System.out.println(list);
+        
+        // trimResults() 去前后空格
+        list = Splitter.on(";").trimResults().splitToList("  a;b  ; c ");
+        System.out.println(list);
+        
+        // omitEmptyStrings() 去除结果为空格的内容
+        list = Splitter.on(";").omitEmptyStrings().splitToList("a;;b;c;");
+        System.out.println(list);
+        
+        // 转为map
+        Map<String, String> map = Splitter.on("&").withKeyValueSeparator("=")
+                .split("name=cck&password=12345");
+        System.out.println(map);
+    }
+    
+}
