fe1e249300d1737b0e65571ce255f8c8d5cee491
luolanmeet <mainem@163.com>
2019-10-21 18:13:42 +0800

    增加 spring mybatis demo

---
+++ orm/mybatis/spring-mybatis/src/main/java/pers/Main.java
@@ -0,0 +1,16 @@
+package pers;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pers.mapper.IUserMapper;
+
+@MapperScan("pers.mapper")
+public class Main {
+    
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        IUserMapper userMapper = applicationContext.getBean(IUserMapper.class);
+        System.out.println(userMapper.findById(1));
+    }
+
+}
---
+++ orm/mybatis/spring-mybatis/src/main/java/pers/bean/User.java
@@ -0,0 +1,20 @@
+package pers.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 
+ * @author cck
+ */
+@Getter
+@Setter
+@ToString
+public class User {
+    
+    private Integer userId;
+    private String name;
+    private String address;
+    
+}
