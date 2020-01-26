60847b93a1be8ac380c4268c58271c579472e743
luolanmeet <mainem@163.com>
2020-01-26 23:00:12 +0800

    增加biMap

--- guava/src/main/java/com/collection/TestBiMap.java
+++ guava/src/main/java/com/collection/TestBiMap.java
@@ -3,11 +3,28 @@ package com.collection;
 import com.google.common.collect.HashBiMap;
 import org.junit.jupiter.api.Test;
 
+/**
+ * BiMap， 可以反转的 Map，可以通过 key 找 value，
+ * 也可以通过 value 找 key
+ */
 public class TestBiMap {
 
     @Test
     public void test1() {
         HashBiMap<String, String> biMap = HashBiMap.create();
+
+        biMap.put("a", "A");
+
+        String val = biMap.get("a");
+        System.out.println(val);
+
+        // inverse 反转
+        val = biMap.inverse().get("A");
+        System.out.println(val);
+
+        // 经过反转后不影响原来的map
+        val = biMap.get("a");
+        System.out.println(val);
     }
 
 }
