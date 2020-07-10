f2f293b47a38253510a714660fcc2101ed529ad5
luolanmeet <mainem@163.com>
2020-07-10 13:53:14 +0800

    IGNORE

--- configuration-center/apollo/springboot-apollo/src/main/java/pers/Application.java
+++ configuration-center/apollo/springboot-apollo/src/main/java/pers/Application.java
@@ -9,7 +9,11 @@ import pers.config.AppConfig2;
 
 import java.util.concurrent.TimeUnit;
 
-// 开启apollo支持
+/**
+ *  开启apollo支持
+ *  https://github.com/ctripcorp/apollo/wiki/Java%E5%AE%A2%E6%88%B7%E7%AB%AF%E4%BD%BF%E7%94%A8%E6%8C%87%E5%8D%97
+ *  用 apollo.bootstrap.enabled = true 开启也行
+ */
 @EnableApolloConfig
 @SpringBootApplication
 public class Application {
