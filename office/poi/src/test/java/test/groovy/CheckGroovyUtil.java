665795d30b88601e8fd78fe515ac92693305a1cb
chenken <mainem@163.com>
2021-10-15 13:58:36 +0800

    补充demo

---
+++ office/poi/src/test/java/test/groovy/CheckGroovyUtil.java
@@ -0,0 +1,85 @@
+package test.groovy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @auther ken.ck
+ * @date 2021/10/15 12:34
+ */
+public class CheckGroovyUtil {
+
+    private static final Pattern PATTERN = Pattern.compile(".*=.*(\\[:\\]|\\[\\])");
+
+    private static final String DEF = "def ";
+    private static final String IGNORE_ONE = "//";
+    private static final String IGNORE_TWO = "/*";
+
+    public static List<String> check(String groovy) {
+        String[] groovyLines = groovy.split("\n");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < groovyLines.length; i++) {
+            if (checkLine(groovyLines[i])) {
+                list.add("line:" + (i + 1) + " " + groovyLines[i]);
+            }
+        }
+
+        return list;
+    }
+
+    private static boolean checkLine(String groovyLine) {
+
+        groovyLine = groovyLine.trim();
+        Matcher matcher = PATTERN.matcher(groovyLine);
+
+        return matcher.find() && !groovyLine.startsWith(DEF)
+                && !groovyLine.startsWith(IGNORE_ONE)
+                && !groovyLine.startsWith(IGNORE_TWO);
+    }
+
+
+    public static void main(String[] args) {
+//        String groovyLine = "def orderLineMap =     [:];";
+//        System.out.println(checkLine(groovyLine));
+
+        String groovy = "\"import com.fasterxml.jackson.databind.ObjectMapper\n" +
+                "\n" +
+                "/**\n" +
+                " * @author kwokhung.lee* @Classname Demo* @Description TODO* @Date 2020/2/12 10:20\n" +
+                " * @Created by wb-lgx631785@cainiao.com\n" +
+                " * 转换插件实现方法\n" +
+                " * 编排引擎会调用 execute\n" +
+                " */\n" +
+                "def execute(v,f = 'JSON'){\n" +
+                "    def map = ['value':'', 'format':'JSON','success':false,'error':'init']\n" +
+                "\n" +
+                "    // 解析xml\n" +
+                "    def response = new XmlSlurper().parseText(v);\n" +
+                "    def target = [:]\n" +
+                "\n" +
+                "    def flag = response.flag?.text()\n" +
+                "    if (flag.equals('success')){\n" +
+                "        target.put('success',true)\n" +
+                "        target.put('errCode','')\n" +
+                "        target.put('errDesc','')\n" +
+                "    }else{\n" +
+                "        target.put('success',false)\n" +
+                "        target.put('errCode',response.code?.text())\n" +
+                "        target.put('errDesc',response.message?.text())\n" +
+                "    }\n" +
+                "\n" +
+                "    // 生成target报文\n" +
+                "    ObjectMapper mapper = new ObjectMapper()\n" +
+                "    map.put('value',mapper.writeValueAsString(target))\n" +
+                "\n" +
+                "    map.put('success',true)\n" +
+                "    return map\n" +
+                "}\"";
+
+        List<String> checkResult = CheckGroovyUtil.check(groovy);
+        checkResult.forEach(System.out::println);
+    }
+
+}
---
+++ office/poi/src/test/java/test/groovy/Tmp4Groovy.java
@@ -0,0 +1,60 @@
+package test.groovy;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @auther ken.ck
+ * @date 2021/10/15 12:54
+ */
+public class Tmp4Groovy {
+
+    public static void main(String[] args) throws Exception {
+
+        String[] filePaths = {
+                "/Users/chenken/Downloads/执行结果1.xlsx",
+                "/Users/chenken/Downloads/执行结果1 (1).xlsx",
+                "/Users/chenken/Downloads/执行结果1 (2).xlsx",
+        };
+
+        for (String filePath : filePaths) {
+            checkoutGroovy(filePath);
+        }
+    }
+
+    public static void checkoutGroovy(String filePath) throws Exception {
+
+        InputStream is = new FileInputStream(filePath);
+        XSSFWorkbook wb = new XSSFWorkbook(is);
+        XSSFSheet sheet = wb.getSheetAt(0);
+
+        // 遍历所有行
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+
+            XSSFRow row = sheet.getRow(i);
+            // 脚本内容在第x列  索引从0开始计算
+            XSSFCell cell = row.getCell(4);
+            String groovy = cell.getStringCellValue();
+
+            List<String> checkResult = CheckGroovyUtil.check(groovy);
+
+            if (checkResult.isEmpty()) {
+                continue;
+            }
+
+            String groovyId = row.getCell(0).getStringCellValue();
+            String groovyName = row.getCell(1).getStringCellValue();
+
+            System.out.println("----脚本id：" +groovyId + "------脚本名称：" + groovyName+"------");
+            checkResult.forEach(System.out::println);
+            System.out.println();
+        }
+    }
+
+}
