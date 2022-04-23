321e4fbddff2e8c659eb2a574b873f65ef7c611e
chenken <mainem@163.com>
2022-04-23 11:03:45 +0800

    ignore

---
+++ distributed-transaction/seata/README.md
@@ -0,0 +1,3 @@
+
+Seata配置: https://github.com/seata/seata/tree/1.4.0/script/client
+SeataDemo: https://github.com/seata/seata-samples
---
+++ distributed-transaction/seata/seata-spring-boot-starter-demo/src/main/java/com/pers/Application.java
@@ -0,0 +1,16 @@
+package com.pers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther ken.ck
+ * @date 2022/4/20 07:41
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
