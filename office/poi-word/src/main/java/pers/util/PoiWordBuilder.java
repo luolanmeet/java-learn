fa418f32d5529171ce33a94389ede72a90a10eb9
chenken <mainem@163.com>
2022-12-08 19:37:40 +0800

    word补充添加表格逻辑

--- office/poi-word/src/main/java/pers/util/PoiWordBuilder.java
+++ office/poi-word/src/main/java/pers/util/PoiWordBuilder.java
@@ -1,13 +1,12 @@
 package pers.util;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,6 +18,9 @@ public class PoiWordBuilder {
     public final static String TEMPLATE_TITLE = "TEMPLATE_TITLE";
     public final static String TEMPLATE_HEAD_1 = "TEMPLATE_HEAD_1";
     public final static String TEMPLATE_HEAD_2 = "TEMPLATE_HEAD_2";
+    public final static String TEMPLATE_HEAD_3 = "TEMPLATE_HEAD_3";
+    public final static String TEMPLATE_TABLE_HEAD_CELL = "TEMPLATE_TABLE_HEAD_CELL";
+    public final static String TEMPLATE_TABLE_DATA_CELL = "TEMPLATE_TABLE_DATA_CELL";
     public final static String TEMPLATE_BODY = "TEMPLATE_BODY";
 
     private Map<String, TemplateStyle> templateMap = new HashMap<>();
@@ -28,8 +30,25 @@ public class PoiWordBuilder {
     public PoiWordBuilder(String templatePath) {
 
         try (FileInputStream in = new FileInputStream(templatePath);) {
+
             doc = new XWPFDocument(in);
-            // 解析模板文件中的格式
+
+            // 保存表格数据格式
+            List<XWPFTable> tables = doc.getTables();
+            if (tables != null && tables.size() > 0) {
+                XWPFTable table = tables.get(0);
+                List<XWPFTableRow> rows = table.getRows();
+                if (rows != null && rows.size() > 0) {
+                    XWPFTableRow row = rows.get(0);
+                    saveTableCellStyle(row, TEMPLATE_TABLE_HEAD_CELL);
+                }
+                if (rows != null && rows.size() > 1) {
+                    XWPFTableRow row = rows.get(1);
+                    saveTableCellStyle(row, TEMPLATE_TABLE_DATA_CELL);
+                }
+            }
+
+            // 保存段落数据格式
             while (doc.getParagraphs().size() > 0) {
                 XWPFParagraph paragraph = doc.getParagraphs().get(0);
                 String text = paragraph.getText();
@@ -44,18 +63,76 @@ public class PoiWordBuilder {
         }
     }
 
-    public void write(String savePath) {
-        try (FileOutputStream out = new FileOutputStream(savePath)) {
-            doc.write(out);
-            doc.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void saveTableCellStyle(XWPFTableRow headRow, String templateStyleKey) {
+        List<XWPFTableCell> tableCells = headRow.getTableCells();
+
+        if (tableCells == null || tableCells.isEmpty()) {
+            return ;
         }
+
+        XWPFTableCell xwpfTableCell = tableCells.get(0);
+        List<XWPFParagraph> paragraphs = xwpfTableCell.getParagraphs();
+        if (paragraphs == null || paragraphs.isEmpty()) {
+            return ;
+        }
+
+        XWPFParagraph xwpfParagraph = paragraphs.get(0);
+        templateMap.put(templateStyleKey, new TemplateStyle(xwpfParagraph));
     }
 
     public PoiWordBuilder addBlankLine() {
         doc.createParagraph();
         return this;
+    }
+
+    public PoiWordBuilder addTable(String [] tableHead, List<String[]> tableDatas) {
+
+        TemplateStyle headRowStyle = templateMap.get(TEMPLATE_TABLE_HEAD_CELL);
+        TemplateStyle dataRowStyle = templateMap.get(TEMPLATE_TABLE_DATA_CELL);
+
+        // 设置表头数据
+        XWPFTable table = doc.createTable();
+        XWPFTableRow headRow = table.getRow(0);
+        for (int i = 0; i < tableHead.length; i++) {
+            XWPFTableCell cell = null;
+            if (i == 0) {
+                cell = headRow.getCell(0);
+            } else {
+                cell = headRow.addNewTableCell();
+            }
+            cell.setText(tableHead[i]);
+            XWPFRun xwpfRun = cell.getParagraphs().get(0).getRuns().get(0);
+            setXwpfRunStyle(xwpfRun, headRowStyle);
+        }
+
+        // 设置表数据
+        for (String[] tableData : tableDatas) {
+            XWPFTableRow dataRow = table.createRow();
+            for (int i = 0; i < tableData.length; i++) {
+                XWPFTableCell cell = dataRow.getCell(i);
+                cell.setText(tableData[i]);
+                XWPFRun xwpfRun = cell.getParagraphs().get(0).getRuns().get(0);
+                setXwpfRunStyle(xwpfRun, dataRowStyle);
+            }
+        }
+
+        return this;
+    }
+
+    private void setXwpfRunStyle(XWPFRun xwpfRun, TemplateStyle templateStyle) {
+
+        if (templateStyle == null) {
+            return ;
+        }
+
+        if (templateStyle.getFontSize() != null) {
+            xwpfRun.setFontSize(templateStyle.getFontSize());
+        }
+        xwpfRun.setFontFamily(templateStyle.getFontFamily());
+        xwpfRun.setBold(templateStyle.getBold());
+        xwpfRun.setColor(templateStyle.getColor());
+        xwpfRun.setCapitalized(templateStyle.getCapitalized());
+        xwpfRun.setShadow(templateStyle.getShadowed());
     }
 
     public PoiWordBuilder addTextUseTemplateStyle(String text, String templateStylekey) {
@@ -66,18 +143,22 @@ public class PoiWordBuilder {
         paragraph.setAlignment(templateStyle.getAlignment());
         paragraph.setStyle(templateStyle.getStyle());
 
-        XWPFRun run = paragraph.createRun();
-        run.setText(text);
-        if (templateStyle.getFontSize() != null) {
-            run.setFontSize(templateStyle.getFontSize());
-        }
-        run.setFontFamily(templateStyle.getFontFamily());
-        run.setBold(templateStyle.getBold());
-        run.setColor(templateStyle.getColor());
-        run.setCapitalized(templateStyle.getCapitalized());
-        run.setShadow(templateStyle.getShadowed());
+        XWPFRun xwpfRun = paragraph.createRun();
+        xwpfRun.setText(text);
+        setXwpfRunStyle(xwpfRun, templateStyle);
 
         return this;
+    }
+
+    public void write(String savePath) {
+        try (FileOutputStream out = new FileOutputStream(savePath)) {
+            doc.write(out);
+            doc.close();
+            this.doc = null;
+            this.templateMap = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- office/poi-word/src/main/java/pers/util/PoiWordBuilderTest.java
+++ office/poi-word/src/main/java/pers/util/PoiWordBuilderTest.java
@@ -1,5 +1,7 @@
 package pers.util;
 
+import java.util.Arrays;
+
 /**
  * @auther ken.ck
  * @date 2022/12/7 15:57
@@ -9,15 +11,23 @@ public class PoiWordBuilderTest {
     public static void main(String[] args) {
 
         String basePath = System.getProperty("user.dir") + "/office/poi-word/";
-        String templatePath = basePath + "src/main/resources/wordStyle.docx";
+        String templatePath = basePath + "src/main/resources/wordStyle2.docx";
         String savePath = basePath + "poiWord.docx";
         PoiWordBuilder builder = new PoiWordBuilder(templatePath);
 
-        builder.addTextUseTemplateStyle("菜鸟物流科技接口文档", PoiWordBuilder.TEMPLATE_TITLE)
+        builder.addTextUseTemplateStyle("xxxx科技接口文档", PoiWordBuilder.TEMPLATE_TITLE)
                 .addBlankLine()
                 .addTextUseTemplateStyle("店铺信息批量查询 oms.shop.batch.query", PoiWordBuilder.TEMPLATE_HEAD_1)
                 .addTextUseTemplateStyle("接口版本：2022年12月05日-2", PoiWordBuilder.TEMPLATE_BODY)
                 .addTextUseTemplateStyle("请求参数", PoiWordBuilder.TEMPLATE_HEAD_2);
+
+        builder.addTable(
+                new String[]{"参数名","参数语义","类型","是否必填","字段长度限制","默认值"},
+                Arrays.asList(
+                        new String[]{"shopCodes","店铺编码","LIST<STRING>","否","0",""},
+                        new String[]{"tenantId","租户id","STRING","否","0","1"},
+                        new String[]{"status","状态","STRING","否","0",""},
+                        new String[]{"viewCode","模型请求唯一码","STRING","否","0","shop_base_manage"}));
 
         builder.write(savePath);
     }
