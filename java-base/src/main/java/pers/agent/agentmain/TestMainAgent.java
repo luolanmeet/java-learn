f96bff73a2e89318435222400a6b096a35510f59
luolanmeet <mainem@163.com>
2022-12-31 11:13:54 +0800

    调整项目结构

--- java-base/src/main/java/pers/agent/agentmain/TestMainAgent.java
+++ java-base/src/main/java/pers/agent/agentmain/TestMainAgent.java
@@ -1,22 +1,25 @@
-package pers.agent.agentmain;
-
-import com.sun.tools.attach.VirtualMachine;
-import com.sun.tools.attach.VirtualMachineDescriptor;
-
-import java.util.List;
-
-/**
- * @auther ken.ck
- * @date 2022/5/5 23:55
- */
-public class TestMainAgent {
-
-    public static void main(String[] args) throws Exception {
-        //正在运行的java 程序 ps id
-        VirtualMachine vm = VirtualMachine.attach("17529");
-//        VirtualMachine vm = VirtualMachine.attach(args[0]);
-        //刚刚编译好的agent jar 位置
-        vm.loadAgent("/Users/chenken/dev/project/learn/cck/java-learn/java-base/target/java-base-1.0-SNAPSHOT.jar");
-    }
-
-}
+//package pers.agent.agentmain;
+//
+//import com.sun.tools.attach.VirtualMachine;
+//import com.sun.tools.attach.VirtualMachineDescriptor;
+//
+//import java.util.List;
+//
+///**
+// * @auther ken.ck
+// * @date 2022/5/5 23:55
+// */
+//public class TestMainAgent {
+//
+//    public static void main(String[] args) throws Exception {
+//
+//        // 依赖，需要在maven pom.xml 中指定
+//
+//        //正在运行的java 程序 ps id
+//        VirtualMachine vm = VirtualMachine.attach("17529");
+////        VirtualMachine vm = VirtualMachine.attach(args[0]);
+//        //刚刚编译好的agent jar 位置
+//        vm.loadAgent("/Users/chenken/dev/project/learn/cck/java-learn/java-base/target/java-base-1.0-SNAPSHOT.jar");
+//    }
+//
+//}
